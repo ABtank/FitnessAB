2359133c1dd13fabda8c255fd145a58ac86be514
Abramov Iurii <abtank@outlook.com>
2021-10-09 06:02:25 +0300

    Composite = | Компоновщик Тренировки |

--- admin-ui/src/main/java/ru/abtank/fitnessab/AdminUiApplication.java
+++ admin-ui/src/main/java/ru/abtank/fitnessab/AdminUiApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.abtank.fitnessab.composite.Round;
+import ru.abtank.fitnessab.composite.Workout;
+import ru.abtank.fitnessab.composite.WorkoutComponent;
+import ru.abtank.fitnessab.composite.WorkoutExercise;
 
 @SpringBootApplication
 public class AdminUiApplication {
@@ -17,6 +21,62 @@ public class AdminUiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminUiApplication.class, args);
+
+		WorkoutComponent workout = new Workout("ferst workout");
+		WorkoutComponent we1 = new WorkoutExercise("Подтягивания");
+		WorkoutComponent we2 = new WorkoutExercise("Отжимания");
+		WorkoutComponent we3 = new WorkoutExercise("Приседания");
+
+		workout.addChild(we1);
+		workout.addChild(we2);
+		workout.addChild(we3);
+
+		WorkoutComponent r11 = new Round("70", 7);
+		WorkoutComponent r12 = new Round("70", 7);
+		WorkoutComponent r13 = new Round("70", 7);
+		WorkoutComponent r14 = new Round("70", 7);
+		WorkoutComponent r15 = new Round("70", 7);
+		WorkoutComponent r21 = new Round("55", 12);
+		WorkoutComponent r22 = new Round("55", 12);
+		WorkoutComponent r23 = new Round("55", 12);
+		WorkoutComponent r24 = new Round("55", 12);
+		WorkoutComponent r25 = new Round("55", 12);
+		WorkoutComponent r31 = new Round("78", 15);
+		WorkoutComponent r32 = new Round("78", 15);
+		WorkoutComponent r33 = new Round("78", 15);
+		WorkoutComponent r34 = new Round("78", 15);
+		WorkoutComponent r35 = new Round("78", 15);
+
+		we1.addChild(r11);
+		we1.addChild(r12);
+		we1.addChild(r13);
+		we1.addChild(r14);
+		we1.addChild(r15);
+		we2.addChild(r21);
+		we2.addChild(r22);
+		we2.addChild(r23);
+		we2.addChild(r24);
+		we2.addChild(r25);
+		we3.addChild(r31);
+		we3.addChild(r32);
+		we3.addChild(r33);
+		we3.addChild(r34);
+		we3.addChild(r35);
+
+		System.out.println("-=Workout=-");
+        System.out.println(workout.toString());
+		System.out.println("Тоннаж за тренировку = "+workout.getSumWeigth());
+		System.out.println("Всего упражнений = "+workout.geSumChild());
+		System.out.println("Упражнения ");
+		int r = 0;
+		for (int i = 0; i < workout.getChild().size(); i++) {
+			System.out.println(" #"+workout.getChild().get(i).getOrdinal()+" "+workout.getChild().get(i).getName());
+			System.out.println(workout.getChild().get(i).getSumWeigth());
+			System.out.println(workout.getChild().get(i).getSumRep());
+			System.out.println(workout.getChild().get(i).getOneRepMax());
+			r+=workout.getChild().get(i).geSumChild();
+		}
+		System.out.println("Всего подходов = "+ r);
 
 //		System.out.println("/////////////SerialVersionUID///////////////");
 //		System.out.println("category "+ ObjectStreamClass.lookup(Category.class).getSerialVersionUID());
