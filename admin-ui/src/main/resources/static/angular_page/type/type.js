511cbb6610b1ba8caa56f55695ca03c69a1de823
Abramov Yurii <abtank@outlook.com>
2021-10-24 22:22:13 +0300

    angular ADD Update Form on page Type

--- admin-ui/src/main/resources/static/angular_page/type/type.js
+++ admin-ui/src/main/resources/static/angular_page/type/type.js
@@ -18,13 +18,40 @@ angular.module('app').controller('typeController', function ($scope, $http) {
                 $scope.fillTable();
             });
     };
-    $scope.submitDeleteType = function () {
-        console.log(contextPath + '/api/v1/type/'+ $scope.delType.id);
-        $http.delete(contextPath + '/api/v1/type/'+ $scope.delType.id)
+    $scope.submitUpdateType = function () {
+        console.log($scope.updType);
+        $http.put(contextPath + '/api/v1/type', $scope.updType)
             .then(function (response) {
+                $scope.updType = null;
+                $("#updId").val(null);
                 $scope.fillTable();
             });
     };
+
+    $scope.setUpdType = function(id){
+        $("#updId").val(id);
+        $scope.fillFormUpdate();
+    }
+
+    $scope.fillFormUpdate = function (){
+        $http.get(contextPath + '/api/v1/type/'+ $("#updId").val())
+            .then(function (response) {
+                $scope.updType = response.data;
+                console.log( $scope.updType);
+            });
+    }
+
+    $scope.submitDeleteType = function () {
+        console.log(contextPath + '/api/v1/type/'+ $scope.delType.id);
+        $scope.deleteType($scope.delType.id);
+    };
+
+    $scope.deleteType = function (id){
+        $http.delete(contextPath + '/api/v1/type/' + id)
+            .then(function (response) {
+                $scope.fillTable();
+            });
+    }
 
     $('#nav_header').find('li').removeClass('active');
     $('#nav_type').addClass('active');
